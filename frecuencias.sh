db9a429ff8f83559e79bce585e2779c7d009b986
macc1100001 <miguelcanul97@hotmail.com>
2023-12-02 11:51:14 -0600

    agregados los textos

--- frecuencias.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# El primer argumento del script es el nombre del archivo del cual se van a calcular las frecuencias
-# la expresion regular en el comando 'sed' separa todo el texto en lineas de 1 caracter
-cat $1 | sed -E 's/(.)/\1\n/g' | sort | uniq -c	| column
-
