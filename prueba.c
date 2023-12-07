cbe55ebb2848f3ba4b88ed365908bc99806336a0
macc1100001 <miguelcanul97@hotmail.com>
2023-12-07 02:01:14 -0600

    modificado el archivo de pruebas

--- prueba.c
+++ prueba.c
@@ -5,14 +5,14 @@
 
 #include<unistd.h>
 
+// calcula el valor entero correspondiente a la fila de la matriz H en binario
 int* calcularFilas(int r, int n){
 	int *numt, k;
 	numt = calloc(r, sizeof(int));
 	for(int j = 0; j < r; ++j){
 		k = 0;
-		for(int i = n; i > 0; --i){
+		for(int i = n; i > 0; --i)
 			numt[j] |= ((i >> j) & 1) << k++;
-		}
 	}
 	return numt;
 }
@@ -27,19 +27,23 @@ unsigned char peso(unsigned char a){
 }
 
 int main(int argc, char** argv){
-	int r = atoi(argv[1]);
-	int n = atoi(argv[2]);
-	int p = atoi(argv[3]);
+	int r = atoi(argv[1]); // delta
+	//int n = atoi(argv[2]);
+	int n = (1 << r) - 1; // 2^r - 1
+	int k = n-r; // 2^r - 1 - r
+	int p = atoi(argv[2]); // z, mensaje
 	int res = 0;
 	int *filas;
 
-	printf("n = %d, r = %d\n", n , r);
+	printf("n = %d, k = %d, r = %d\n", n , k, r);
 	printf("p = %x\n", p);
 	filas = calcularFilas(r, n);
 	for(int i = 0; i < r; ++i){
 		printf("%d ", filas[i]);
 		printf("peso = %d\n", peso(filas[i] & p) % 2);
-		res |= (peso(filas[i] & p) % 2) << i;
+		// multiplicacion por cada fila de la matriz y se suman los 1's
+		// Hz' = res
+		res |= (peso(filas[i] & p) % 2) << i; // se forma el entero correspondiente al bit a corregir
 	}
 	printf("res = %d\n", res);
 	if(res)
