8da125f2a30267b4a0b9955aef3647f2f858e907
macc1100001 <miguelcanul97@hotmail.com>
2023-12-08 16:58:26 -0600

    Agregadas algunas pruebas para poder realizar la decodificacion y codificacion

--- prueba.c
+++ prueba.c
@@ -3,6 +3,9 @@
 #include<string.h>
 #include<stdbool.h>
 
+
+static int r, k, n;
+
 /*	Construir L, el arreglo que contiene todos los codewords
 	por ahora el arreglo es fijo, pero se quiere que este arreglo
 	se genere de manera dinamica , utilizando manipulaciones
@@ -15,6 +18,14 @@ static unsigned char L[] = { 0, 105, 42, 67,
 						 	76, 37, 102, 15,
 						 	112, 25, 90, 51,
 						 	60, 85, 22, 127 };
+						 	
+int decodificarDato(int t, int tam){
+	for(int i = 0; i < tam; ++i)
+		if(L[i] == t)
+			return i;
+	return -1;
+}
+
 
 // calcula el valor entero correspondiente a la fila de la matriz H en binario
 void calcularFilas(int* numt, int r, int n){
@@ -57,16 +68,19 @@ int decodificar(int r, int n, int p){
 		printf("Hay un error en el bit %d\n", res);
 		corregido = p ^ (1 << (n-res));
 		printf("Corregido = %d\n", corregido);
-		return corregido;
+		int decodificado = decodificarDato(corregido, (r << k));
+		if(decodificado == -1)
+			return p;
+		return decodificado;
 	}
 	printf("No errores\n");
-	return -1;
+	return decodificarDato(p, (r << k));
 }
 
 int main(int argc, char** argv){
-	int r = atoi(argv[1]); // delta
-	int n = (1 << r) - 1; // 2^r - 1
-	int k = n-r; // 2^r - 1 - r
+	r = atoi(argv[1]); // delta
+	n = (1 << r) - 1; // 2^r - 1
+	k = n-r; // 2^r - 1 - r
 	int p = atoi(argv[2]); // z, mensaje
 	//int *filas;
 
@@ -102,6 +116,8 @@ int main(int argc, char** argv){
 	decodificar(r, n, p);
 	int dato1 = decodificar(r, n, codigo1 ^ 8);
 	int dato2 = decodificar(r, n, codigo2 ^ 4);
+	printf("dato1 = %d = 0x%x\n", dato1, dato1);
+	printf("Corregido final: %d = 0x%x\n", (dato2 << 4) | dato1, (dato2 << 4) | dato1);
 	/*
 	printf("Recibido: %d = 0x%x\n", p, p);
 	int filas[r];
